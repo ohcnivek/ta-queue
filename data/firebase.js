f5e500118e974c5f3defa55c45870242f284c97d
ohcnivek <64056046+ohcnivek@users.noreply.github.com>
2022-03-31 15:17:48 -0400

    adding posting functionality

--- data/firebase.js
+++ data/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, doc, onSnapshot } from "firebase/firestore";
+import { getFirestore, doc, onSnapshot, collection, addDoc } from "firebase/firestore";
 import {EXPO_APP_API_KEY,EXPO_APP_PROJECT } from '@env'
+
 
 
 
@@ -16,5 +17,19 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+const COLLECTION = "queue-questions"
 
+async function post(desc, groupMem, name, privateBool, question, status) {
+    const docRef = await addDoc(collection(db, COLLECTION), {
+        desc: desc,
+        groupMem: groupMem,
+        name: name , 
+        privateBool: privateBool , 
+        question: question , 
+        status: status,
+      });
 
+      console.log("Document written with ID: ", docRef.id);
+}
+
+// post("post", [], "post kevin", true, "post question", "1 min post")
