04af998a8c413a52c74617eb27c36b680c93b20c
sumeine <sumeine@gmail.com>
2022-04-29 14:28:38 -0400

    join queue page looks better now

--- screens/JoinQueueScreen.js
+++ screens/JoinQueueScreen.js
@@ -16,16 +16,17 @@ function JoinQueueScreen(props, {navigation}) {
     const [meetingLinkUserText, setMeetingLinkUserText] = useState("");
     const uid = props.route.params.uid; 
     return (
-        <View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start', backgroundColor: '#FFF5ED',}}>
-            <Input placeholder='Name'  onChangeText={(text) => setNameUserText(text)}/>
-            <Input placeholder='Question' onChangeText={(text) => setQuestionUserText(text)}/>
-            <Input placeholder='Description'  onChangeText={(text) => setDescriptionUserText(text)}/>
+        <View style={{ flex: 1, backgroundColor: '#FFF5ED',}}>
+            <Input style={{ fontSize: 18, fontFamily: 'IBMPlexMono-Regular'}} placeholder='Name'  onChangeText={(text) => setNameUserText(text)}/>
+            <Input style={{ fontSize: 18, fontFamily: 'IBMPlexMono-Regular'}} placeholder='Question' onChangeText={(text) => setQuestionUserText(text)}/>
+            <Input style={{ fontSize: 18, fontFamily: 'IBMPlexMono-Regular'}} placeholder='Description'  onChangeText={(text) => setDescriptionUserText(text)}/>
 
             <Text style={{ fontSize: 18, fontFamily: 'IBMPlexMono-Regular', marginLeft: 10 }}>In-Person or Virtual?</Text>
             <View style={{flexDirection: 'row', marginLeft: "1%"}}>
               <CheckBox
                 title="In-Person"
                 checked={checkedInPerson}
+                fontFamily='IBMPlexMono-Regular'
                 onPress={() => {
                   if (checkedInPerson === false && checkedVirtual === false) {
                     toggleCheckedInPerson(!checkedInPerson)
@@ -38,6 +39,7 @@ function JoinQueueScreen(props, {navigation}) {
               <CheckBox
                 title="Virtual"
                 checked={checkedVirtual}
+                fontFamily='IBMPlexMono-Regular'
                 onPress={() => {
                   if (checkedInPerson === false && checkedVirtual === false) {
                     toggleCheckedVirtual(!checkedVirtual)
@@ -49,12 +51,13 @@ function JoinQueueScreen(props, {navigation}) {
               /> 
             </View>
 
-            <Input placeholder='Meeting Link for Virtual Office Hours' onChangeText={(text) => setMeetingLinkUserText(text)}/>
+            <Input style={{ fontSize: 17, fontFamily: 'IBMPlexMono-Regular'}} placeholder='Meeting Link for Virtual Office Hours' onChangeText={(text) => setMeetingLinkUserText(text)}/>
             <Text style={{ fontSize: 18, fontFamily: 'IBMPlexMono-Regular', marginLeft: 10 }}>Private or Public?</Text>
             <View style={{flexDirection: 'row', marginLeft: "1%"}}>
               <CheckBox
                 title="Public"
                 checked={checkedPublic}
+                fontFamily='IBMPlexMono-Regular'
                 onPress={() => {
                   if (checkedPublic === false && checkedPrivate === false) {
                     toggleCheckedPublic(!checkedPublic)
@@ -67,6 +70,7 @@ function JoinQueueScreen(props, {navigation}) {
               <CheckBox
                 title="Private"
                 checked={checkedPrivate}
+                fontFamily='IBMPlexMono-Regular'
                 onPress={() => {
                   if (checkedPublic === false && checkedPrivate === false) {
                     toggleCheckedPrivate(!checkedPrivate)
@@ -95,42 +99,17 @@ const styles = StyleSheet.create({
     contentView: {
       flex: 1,
     },
-    title: {
-      justifyContent: "flex-end",
-      alignItems: "flex-end",
-      fontSize: 30,
-      color: '#6b635b',
-      flex: 1,
-      paddingTop: 50,
-      //marginTop: 100, margin is outside of block, padding is in
-    },
     joinQueueButton: {
-      marginRight:40,
-      marginLeft:40,
-      marginTop:10,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginRight:140,
+      marginLeft:140,
+      marginTop:50,
       padding:10,
       backgroundColor:'#C4A484',
       borderRadius:10,
       borderWidth: 1,
       borderColor: '#fff'
-    },
-    textPrimary: {
-      marginVertical: 20,
-      textAlign: 'center',
-      fontSize: 20,
-    },
-    textSecondary: {
-      marginBottom: 10,
-      textAlign: 'center',
-      fontSize: 17,
-    },
-    subHeader: {
-      backgroundColor : "#FCEAD7",
-      color : "red",
-      fontSize : 100,
-      textAlign : "center",
-      paddingVertical : 5,
-      marginBottom : 10
     }
   });
 
