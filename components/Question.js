ced79a24cf9cd081d624a03719b4bb763b89d8f2
Kelly Tran <kellytran3k@gmail.com>
2022-04-30 00:15:40 -0400

    updated student visibility in regards to private/in progress questions
    make private question visible for owner
    
    make in progress available for all people

--- components/Question.js
+++ components/Question.js
@@ -10,31 +10,36 @@ class Question extends Component {
       
       <Button 
           onPress= {() => {
-            if (this.props.privateBool && this.props.isStudent || this.props.status === "In Progress" && this.props.isStudent) {
-              //  its private
-              if (this.props.privateBool) {
-                Alert.alert("Can't join a private question!")
-              } else {
-                Alert.alert("Can't join a question that's already in progress!")
-              }
-            } else {
               if (this.props.isStudent) {
-                console.log(this.props.uidArray)
                 if (this.props.uid == this.props.uidArray[0]) {
-                  this.props.navigation.navigate('Requests Screen', {docID: this.props.docID, meetingLink: this.props.meetingLink})
-                }
-                else if (this.props.uidArray.includes(this.props.uid)) {
-                  // student owns or has already joined question
+                  // student is owner of question
+                  if (this.props.privateBool) {
+                    // student owner's question is private
+                    this.props.navigation.navigate('Meeting Link', {docID: this.props.docID, meetingLink: this.props.meetingLink})
+                  } else {
+                    // student owner's question is public
+                    this.props.navigation.navigate('Requests Screen', {docID: this.props.docID, meetingLink: this.props.meetingLink})
+                  }
+                } else if (this.props.uidArray.includes(this.props.uid)) {
+                  // student is a member of this question
                   this.props.navigation.navigate('Meeting Link', {docID: this.props.docID, meetingLink: this.props.meetingLink})
                 } else {
-                  //student wants to join question
-                  this.props.navigation.navigate('Join Question', {docID: this.props.docID, uid: this.props.uid})
+                  // student is not a member of this question
+                  if (this.props.status === "In Progress") {
+                    // student cannot join because question is in progress
+                    Alert.alert("Can't join a question that's already in progress!")
+                  } else if (this.props.privateBool) {
+                    Alert.alert("Can't join a private question!")
+                  } else {
+                    this.props.navigation.navigate('Join Question', {docID: this.props.docID, uid: this.props.uid})
+                  }
                 }
+              // the user is a TA
               } else {
                 this.props.navigation.navigate('Manage Queue', {docID: this.props.docID, pushTokens: this.props.pushTokens, meetingLink: this.props.meetingLink})
               }
             }
-          }}
+          }
           title={
             <CustomTitle
               name = {this.props.name} 
@@ -46,6 +51,8 @@ class Question extends Component {
               privateBool={this.props.privateBool}
               meetingLink={this.props.meetingLink}
               groupMem={this.props.groupMem.join(', ')}
+              uidArray={this.props.uidArray}
+              uid={this.props.uid}
               />
             }
           titleStyle={{ fontWeight: 'bold', fontSize: 38 }}
@@ -76,9 +83,11 @@ const CustomTitle = (props) => {
   const isStudent = props.isStudent;
   const meetingLink = props.meetingLink
   const privateBool = props.privateBool;
+  const uidArray = props.uidArray
+  const uid = props.uid
 
   const publicOrPrivate = privateBool? "private" : "public"
-  if (!privateBool || !isStudent) {
+  if (!privateBool || !isStudent || uidArray.includes(uid)) {
     if (status !== 'Waiting...') {
       return (
         <View style= {styles.contentView}>
