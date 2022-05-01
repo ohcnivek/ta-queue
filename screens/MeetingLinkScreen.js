85996614d22365b13c812b4bcf1a26389e15b7cd
sumeine <sumeine@gmail.com>
2022-04-30 22:36:44 -0400

    meetinglink fix

--- screens/MeetingLinkScreen.js
+++ screens/MeetingLinkScreen.js
@@ -31,19 +31,6 @@ function MeetingLinkScreen(props, {navigation}) {
 
 
 const styles = StyleSheet.create({
-    contentView: {
-      flex: 1,
-    },
-    title: {
-      justifyContent: "flex-end",
-      alignItems: "flex-end",
-      fontSize: 30,
-      color: '#6b635b',
-      flex: 1,
-      paddingTop: 50,
-      //marginTop: 100, margin is outside of block, padding is in
-    },
-<<<<<<< HEAD
     joinQueueButton: {
       justifyContent: 'center',
       alignItems: 'center',
@@ -54,16 +41,7 @@ const styles = StyleSheet.create({
       borderRadius:10,
       borderWidth: 1,
       borderColor: '#fff'
-=======
-    //this is for multiple buttons in a column
-    // buttonsContainer: {
-    //   flexDirection: 'row',
-    //   flexWrap: 'wrap',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   width: '100%',
-    //   marginVertical: 20,
-    // }, 
+    },
     leaveButton: {
       fontSize:232,
       marginLeft:40,
@@ -74,25 +52,6 @@ const styles = StyleSheet.create({
       borderRadius:10,
       borderWidth: 1,
       borderColor: 'red'
->>>>>>> origin
-    },
-    textPrimary: {
-      marginVertical: 20,
-      textAlign: 'center',
-      fontSize: 20,
-    },
-    textSecondary: {
-      marginBottom: 10,
-      textAlign: 'center',
-      fontSize: 17,
-    },
-    subHeader: {
-      backgroundColor : "#FCEAD7",
-      color : "red",
-      fontSize : 100,
-      textAlign : "center",
-      paddingVertical : 5,
-      marginBottom : 10
     }
   });
 
